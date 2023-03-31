852a1ed2380c7fb8061e5e486cd0ed0430256480
Your Name <you@example.com>
2023-03-31 13:49:15 +0300

    update routes

--- app/(business)/activityTime/layout.tsx
+++ app/(business)/activityTime/layout.tsx
@@ -1,19 +1,7 @@
-"use client";
-import { SessionProvider } from "next-auth/react";
 import React from "react";
-import NavBar from "../(navbar)/Navbar";
 
-export default function ActivityTimeLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <>
-      <SessionProvider>
-        <NavBar />
-      </SessionProvider>
-      <div className="block pl-64">{children}</div>
-    </>
-  );
+function Layout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
 }
+
+export default Layout;
