f0fbd15fcaff848ebc1e8a45bd8797ce464a1126
Your Name <you@example.com>
2024-08-17 14:02:55 +0300

    SignInLayout export ddefault

--- app/(auth)/login/layout.tsx
+++ app/(auth)/login/layout.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@lib/auth";
+
 async function SignInLayout({ children }: { children: React.ReactNode }) {
   const session = await getServerSession(authOptions);
+  console.log("session", session);
 
   return (
     <section className="h-screen w-full flex justify-center items-center">
@@ -10,3 +12,5 @@ async function SignInLayout({ children }: { children: React.ReactNode }) {
     </section>
   );
 }
+
+export default SignInLayout;
